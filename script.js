688d686b4921342e5a09357aa0ec2a48e12c0622
manrajg13 <manraj836@gmail.com>
2022-07-21 19:02:23 -0400

    Cart, Settings, Products page, etc

--- script.js
+++ script.js
@@ -4,9 +4,11 @@ window.onload = function () {
 	if (signed_in) {
 		document.getElementById('account').style.display = "block";
 		document.getElementById('sign_in').innerHTML = "<button>SIGN OUT</button>";
+		document.getElementById('sign_up').innerHTML = "";
 	}
 	else {
 		document.getElementById('account').style.display = "none";
 		document.getElementById('sign_in').innerHTML = "<button>SIGN IN</button>";
+		document.getElementById('sign_up').innerHTML = "<button>SIGN UP</button>";
 	}
 }
