5f56b85d0d6f2e163b532661c52eab05a5ae1146
Manraj <manraj836@gmail.com>
2022-08-10 12:54:12 -0400

    Products page

--- demo/src/app/home/home.component.ts
+++ demo/src/app/home/home.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Products } from 'src/app/models/products.model';
+import { ProductService } from 'src/app/services/products.service';
+import { SigninService } from '../services/signin.service';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +10,55 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  products?: Products[];
+
+  constructor(public productService: ProductService, public signinService: SigninService) { }
+
+  product: Products = {
+    name: '',
+    description: '',
+    price: 0
+  };
+  submitted = false;
 
   ngOnInit(): void {
+    this.listProducts();
+  }
+
+  listProducts(){
+    this.productService.getAll()
+      .subscribe(
+        data => {
+          this.products = data;
+          console.log(data);
+        },
+        error => {
+          console.log(error);
+        });
+  }
+
+  getProduct(product: Products){
+    
+  }
+
+  addProduct(){
+    const data = {
+      name: this.product.name,
+      price: this.product.price,
+      description: this.product.description
+    };
+
+    this.productService.create(data)
+      .subscribe(
+        response => {
+          console.log(response);
+          this.submitted = true;
+        },
+        error => {
+          console.log(error);
+        });
+    window.location.reload();   
+    
   }
 
 }
