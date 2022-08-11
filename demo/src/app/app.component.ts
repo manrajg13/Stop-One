c7cf52ef4ef2cbb85fa96ac6de2c494b54ae3c16
Manraj <manraj836@gmail.com>
2022-08-10 21:14:06 -0400

    themes

--- demo/src/app/app.component.ts
+++ demo/src/app/app.component.ts
@@ -4,14 +4,22 @@ import { SigninService } from './services/signin.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css', '../../node_modules/font-awesome/css/font-awesome.css'],
+  styleUrls: ['./app.component.css'],
   encapsulation: ViewEncapsulation.None
 })
 
 export class AppComponent implements OnInit {
   title = 'Stop-One';
+  theme: string;
 
-  constructor (public signinService: SigninService) { }
+  constructor (public signinService: SigninService) {
+    this.theme = localStorage.getItem("theme")!;
+  }
 
-  ngOnInit(): void { }
+  ngOnInit(): void {
+  }
+  
+  getTheme() {
+    return localStorage.getItem("theme")!;
+  }
 }
